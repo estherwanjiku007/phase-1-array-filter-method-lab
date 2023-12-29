5e25b7db017944ff56c1c87337bd1373bd9b014d
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-29 17:22:36 +0300

    aster

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+//let drivers2=drivers.filter((word)=>drivers.
+function findingMatch(collection1,string){
+  for(i=0;i<collection1.length;i++){
+    console.log(i)
+  }
+  while( i===string){
+    return i
+  }
+}
+findingMatch();
+
+
+    function fuzzyMatch(collection2,allString){
+  for (const words of collection2){
+    if(collection2("firstWord")===allString)
+    console.log(collection2);
+  }
+    }
+    fuzzyMatch();
+    function matchName(collection3,myString){
+      collection3.filter((name)=>name===myString);
+        console.log(collection3);
+      }
+matchName();
+
+    
